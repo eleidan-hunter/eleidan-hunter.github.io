7c822bddd8ac67029875c3f19a32ac32f497ced7
eleidan-hunter <48258074+eleidan-hunter@users.noreply.github.com>
2021-05-28 15:44:50 +0300

    Add files via upload

---
+++ dist/main.dev.js
@@ -0,0 +1,80 @@
+"use strict";
+
+var rollDice = 0;
+var RollDiceBtn = document.querySelector('.dice_roll');
+var RiceOutput = document.querySelector('.dice_output');
+var HealthPoint = document.querySelector('.health_max');
+var PlayerCard1 = document.querySelector('.player_1');
+var PlayerCard2 = document.querySelector('.player_2');
+var health = 100;
+HealthPoint.innerHTML = "<div class=\"health_indicator\" style=\"width: ".concat(health, "%;\"></div>");
+RollDiceBtn.addEventListener('click', diceRoll);
+RollDiceBtn.addEventListener('click', changePlayer); // смена игроков после броска
+
+var playerCheck = true;
+
+function changePlayer() {
+  if (playerCheck === true) {
+    PlayerCard1.style.cssText = 'border:2px solid red';
+    PlayerCard2.style.cssText = 'border:2px solid #000';
+    playerCheck = false;
+  } else if (playerCheck === false) {
+    PlayerCard1.style.cssText = 'border:2px solid #000';
+    PlayerCard2.style.cssText = 'border:2px solid red';
+    playerCheck = true;
+  }
+} // === уменьшение очков здоровья по броску кубика
+
+
+function hitResult() {
+  health -= rollDice;
+  HealthPoint.innerHTML = "<div class=\"health_indicator\" style=\"width: ".concat(health, "%;\"></div>");
+
+  if (health <= 0) {
+    alert('GAME OVER');
+    health = 100;
+  }
+} // === генерация броска кубика
+
+
+function diceRoll() {
+  rollDice = Math.round(1 + Math.random() * (6 - 1));
+  console.log('rollDice', rollDice);
+
+  switch (rollDice) {
+    case 1:
+      RiceOutput.style.cssText = 'background: url(img/dice1.svg);';
+      break;
+
+    case 2:
+      RiceOutput.style.cssText = 'background: url(img/dice2.svg);';
+      break;
+
+    case 3:
+      RiceOutput.style.cssText = 'background: url(img/dice3.svg);';
+      break;
+
+    case 4:
+      RiceOutput.style.cssText = 'background: url(img/dice4.svg);';
+      break;
+
+    case 5:
+      RiceOutput.style.cssText = 'background: url(img/dice5.svg);';
+      break;
+
+    case 6:
+      RiceOutput.style.cssText = 'background: url(img/dice6.svg);';
+      break;
+  } // if( player1Check === true){
+  // 	PlayerCard1.style.cssText='border:2px solid red';
+  // 	PlayerCard2.style.cssText='border:2px solid #000';
+  // 	player1Check = false
+  // } else if (player1Check === false){
+  // 	PlayerCard1.style.cssText='border:2px solid #000';
+  // 	PlayerCard2.style.cssText='border:2px solid red';
+  // 	player1Check = true;
+  // }
+
+
+  hitResult(health, rollDice);
+}
--- main.js
+++ main.js
@@ -1,57 +1,78 @@
+let rollDice = 0;
+let RollDiceBtn = document.querySelector('.dice_roll');
+let RiceOutput = document.querySelector('.dice_output');
+let HealthPoint = document.querySelector('.health_max');
+let PlayerCard1 = document.querySelector('.player_1')
+let PlayerCard2 = document.querySelector('.player_2')
 
-
-
-// let minDice = 1;
-// let maxDice = 6;
-let rollDice = 1;
-let rollDiceBtn = document.querySelector('.diceRoll');
-let diceOutput = document.querySelector('.diceOutput');
-rollDiceBtn.onclick = diceRoll;
-
-let healthPoint = document.querySelector('.healthMax');
 let health = 100;
-healthPoint.innerHTML = (`<div class="healthIndicator" style="width: ${health}%;"></div>`)
 
-//GENERATE DICE ROLL RESULT
-function diceRoll(){
-	rollDice = Math.round(1 + Math.random() * (6 - 1));
-	console.log('rollDice',rollDice);
+HealthPoint.innerHTML = (`<div class="health_indicator" style="width: ${health}%;"></div>`);
+RollDiceBtn.addEventListener('click', diceRoll);
+RollDiceBtn.addEventListener('click', changePlayer);
 
-	switch(rollDice){
-		case 1:
-			diceOutput.style.cssText=`background: url(img/dice1.svg);`;
-			break;
-		case 2:
-			diceOutput.style.cssText=`background: url(img/dice2.svg);`;
-			break;
-		case 3:
-			diceOutput.style.cssText=`background: url(img/dice3.svg);`;
-			break;
-		case 4:
-			diceOutput.style.cssText=`background: url(img/dice4.svg);`;
-			break;
-		case 5:
-			diceOutput.style.cssText=`background: url(img/dice5.svg);`;
-			break;
-		case 6:
-			diceOutput.style.cssText=`background: url(img/dice6.svg);`;
-			break;
+// смена игроков после броска
+let playerCheck = true;
+
+function changePlayer(){
+	if( playerCheck === true){
+		PlayerCard1.style.cssText='border:2px solid red';
+		PlayerCard2.style.cssText='border:2px solid #000';
+		playerCheck = false
+	} else if (playerCheck === false){
+		PlayerCard1.style.cssText='border:2px solid #000';
+		PlayerCard2.style.cssText='border:2px solid red';
+		playerCheck = true;
 	}
-	
-	hitResult(health, rollDice);
 }
 
-
-
-// BEHAVIOR INCREASE HEALTH POINT
+// === уменьшение очков здоровья по броску кубика
 function hitResult (){
 
 	health -= rollDice
-	healthPoint.innerHTML = (`<div class="healthIndicator" style="width: ${health}%;"></div>`)
+	HealthPoint.innerHTML = (`<div class="health_indicator" style="width: ${health}%;"></div>`)
 	if( health <=0 ){
 		alert('GAME OVER');
 		health = 100;
 	}	
 }
 
-console.log('rollDice',rollDice);
+// === генерация броска кубика
+function diceRoll(){
+	rollDice = Math.round(1 + Math.random() * (6 - 1));
+	console.log('rollDice',rollDice);
+
+	switch(rollDice){
+		case 1:
+			RiceOutput.style.cssText='background: url(img/dice1.svg);';
+			break;
+		case 2:
+			RiceOutput.style.cssText='background: url(img/dice2.svg);';
+			break;
+		case 3:
+			RiceOutput.style.cssText='background: url(img/dice3.svg);';
+			break;
+		case 4:
+			RiceOutput.style.cssText='background: url(img/dice4.svg);';
+			break;
+		case 5:
+			RiceOutput.style.cssText='background: url(img/dice5.svg);';
+			break;
+		case 6:
+			RiceOutput.style.cssText='background: url(img/dice6.svg);';
+			break;
+	}
+		// if( player1Check === true){
+		// 	PlayerCard1.style.cssText='border:2px solid red';
+		// 	PlayerCard2.style.cssText='border:2px solid #000';
+		// 	player1Check = false
+		// } else if (player1Check === false){
+		// 	PlayerCard1.style.cssText='border:2px solid #000';
+		// 	PlayerCard2.style.cssText='border:2px solid red';
+		// 	player1Check = true;
+		// }
+	
+	
+	
+	hitResult(health, rollDice);
+}
